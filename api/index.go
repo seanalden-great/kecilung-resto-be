aac02ba318a92a5e28504fd76cc698b0d62fdc42
Sean Alden <c14210052@john.petra.ac.id>
2026-09-18 21:52:34 +0700

    update

--- api/index.go
+++ api/index.go
@@ -1,8 +1,69 @@
+// package api
+
+// import (
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/seanalden-great/kecilung-resto-be/config"
+// 	httpDelivery "github.com/seanalden-great/kecilung-resto-be/delivery/http"
+// 	"github.com/seanalden-great/kecilung-resto-be/domain"
+// 	"github.com/seanalden-great/kecilung-resto-be/repository"
+// 	"github.com/seanalden-great/kecilung-resto-be/usecase"
+// )
+
+// var app *gin.Engine
+
+// // Fungsi init() otomatis dieksekusi pertama kali oleh Vercel sebelum menerima request
+// func init() {
+// 	// Koneksi Database (menggunakan variabel environment Vercel)
+// 	db := config.ConnectDatabase()
+	
+// 	// Migrasi Tabel
+// 	err := db.AutoMigrate(&domain.Category{}, &domain.Menu{})
+// 	if err != nil {
+// 		log.Println("Gagal migrasi:", err)
+// 	}
+
+// 	// Inisialisasi Arsitektur
+// 	catRepo := repository.NewCategoryRepository(db)
+// 	catUseCase := usecase.NewCategoryUsecase(catRepo)
+
+// 	menuRepo := repository.NewMenuRepository(db)
+// 	menuUseCase := usecase.NewMenuUsecase(menuRepo)
+
+// 	// Set Gin ke mode Release agar log lebih bersih di Vercel
+// 	gin.SetMode(gin.ReleaseMode)
+// 	app = gin.Default()
+
+// 	// Middleware CORS
+// 	app.Use(func(c *gin.Context) {
+// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(204)
+// 			return
+// 		}
+// 		c.Next()
+// 	})
+
+// 	// Daftarkan Routes
+// 	httpDelivery.RegisterHandlers(app, catUseCase, menuUseCase)
+// }
+
+// // Handler adalah fungsi standar yang dicari oleh Vercel
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	// Serahkan request HTTP dari Vercel ke dalam router Gin
+// 	app.ServeHTTP(w, r)
+// }
+
 package api
 
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/seanalden-great/kecilung-resto-be/config"
@@ -14,9 +75,9 @@ import (
 
 var app *gin.Engine
 
-// Fungsi init() otomatis dieksekusi pertama kali oleh Vercel sebelum menerima request
-func init() {
-	// Koneksi Database (menggunakan variabel environment Vercel)
+// Kita buat fungsi inisialisasi terpisah (BUKAN init() bawaan Go)
+func initApp() {
+	// Koneksi Database
 	db := config.ConnectDatabase()
 	
 	// Migrasi Tabel
@@ -32,7 +93,6 @@ func init() {
 	menuRepo := repository.NewMenuRepository(db)
 	menuUseCase := usecase.NewMenuUsecase(menuRepo)
 
-	// Set Gin ke mode Release agar log lebih bersih di Vercel
 	gin.SetMode(gin.ReleaseMode)
 	app = gin.Default()
 
@@ -48,12 +108,25 @@ func init() {
 		c.Next()
 	})
 
+	// Endpoint Debug (Untuk memastikan Vercel benar-benar membaca ENV)
+	app.GET("/api/debug-env", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"host": os.Getenv("DB_HOST"),
+			"port": os.Getenv("DB_PORT"),
+			"user": os.Getenv("DB_USER"),
+		})
+	})
+
 	// Daftarkan Routes
 	httpDelivery.RegisterHandlers(app, catUseCase, menuUseCase)
 }
 
-// Handler adalah fungsi standar yang dicari oleh Vercel
+// Handler ini adalah pintu masuk utama Vercel
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Serahkan request HTTP dari Vercel ke dalam router Gin
+	// Jika aplikasi belum diinisialisasi (request pertama), maka inisialisasi sekarang
+	if app == nil {
+		initApp()
+	}
+	// Teruskan request ke Gin Router
 	app.ServeHTTP(w, r)
 }
--- config/database.go
+++ config/database.go
@@ -1,3 +1,30 @@
+// package config
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// func ConnectDatabase() *gorm.DB {
+// 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+// 		os.Getenv("DB_USER"),
+// 		os.Getenv("DB_PASS"),
+// 		os.Getenv("DB_HOST"),
+// 		os.Getenv("DB_PORT"),
+// 		os.Getenv("DB_NAME"),
+// 	)
+
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatal("Gagal terkoneksi ke database:", err)
+// 	}
+// 	return db
+// }
+
 package config
 
 import (
@@ -10,11 +37,20 @@ import (
 )
 
 func ConnectDatabase() *gorm.DB {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+
+	// 1. Validasi Keberadaan Environment Variables
+	if host == "" || port == "" {
+		log.Fatal("CRITICAL ERROR: DB_HOST atau DB_PORT kosong! Vercel belum menyuntikkan variabel environment.")
+	}
+
+	// 2. Format DSN (Tambahkan &tls=true di bagian belakang khusus untuk Cloud Database)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=true",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
+		host,
+		port,
 		os.Getenv("DB_NAME"),
 	)
 
@@ -22,5 +58,7 @@ func ConnectDatabase() *gorm.DB {
 	if err != nil {
 		log.Fatal("Gagal terkoneksi ke database:", err)
 	}
+	
+	log.Println("Sukses terhubung ke database:", host)
 	return db
 }
