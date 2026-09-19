a9eb88a0e480a87a269d8c6f0c2793a93ca5b84d
Sean Alden <c14210052@john.petra.ac.id>
2026-09-19 11:39:17 +0700

    remove once do in index.go

--- delivery/http/api_handler.go
+++ delivery/http/api_handler.go
@@ -25,7 +25,7 @@ func RegisterHandlers(r *gin.Engine, cu domain.CategoryUsecase, mu domain.MenuUs
 	menu := api.Group("/menus")
 	{
 		menu.GET("", fetchMenus(mu))
-		cat.GET("", getMenusByCategory(mu))
+		menu.GET("", getMenusByCategory(mu))
 		menu.POST("", createMenu(mu))
 		menu.PUT("/:id", updateMenu(mu))
 		menu.DELETE("/:id", deleteMenu(mu))
